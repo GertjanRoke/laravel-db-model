459604a80f8ff0e6461e8ac85b974130e079c5c5
Gertjan Roke <g.a.roke90@gmail.com>
2021-09-15 19:25:07 +0200

    Cleaned up unneeded classes and files
    Signed-off-by: Gertjan Roke <g.a.roke90@gmail.com>

---
+++ src/DbModel.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace GertjanRoke\LaravelDbModel;
+
+class DbModel
+{
+
+}
---
+++ src/LaravelDbModelServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace GertjanRoke\LaravelDbModel;
+
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
+class LaravelDbModelServiceProvider extends PackageServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        /*
+         * This class is a Package Service Provider
+         *
+         * More info: https://github.com/spatie/laravel-package-tools
+         */
+        $package->name('LaravelDbModel');
+    }
+}
